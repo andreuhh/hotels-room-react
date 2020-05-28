3bddeed2925fe8597d274acd7f5f8028889ff53e
Andrea Guffi <andreaguffi@MBP-di-Andrea-2.homenet.telecomitalia.it>
2020-05-28 10:37:57 +0200

    updated acces contentfull

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful'
 
 export default createClient({
-    space: 'kjt16x83wrnj',
-    accessToken: 'd5LWa3LKIeSzVRN8vFkn3P4PMCs3pTbt4uCqvA6AGaw'
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
